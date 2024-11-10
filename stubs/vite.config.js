b7ca73305367659156563197860d916d293860b9
entrepeneur4lyf <shawn.payments@gmail.com>
2024-11-10 12:52:26 -0500

    feat: Add auto-import of Mingle components in Vite config

--- stubs/vite.config.js
+++ stubs/vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+import findMingles from './vendor/ijpatricio/mingle/resources/js/autoImport.js'
+const mingles = findMingles('resources/js')
+// Optional: Output the mingles to the console, for a visual check
+console.log('Auto-importing mingles:', mingles)
+
 export default defineConfig({
     resolve: {
         alias: {
@@ -13,8 +18,7 @@ export default defineConfig({
         react(),
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                ...mingles,
             ],
             refresh: true,
         }),
